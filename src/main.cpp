e8a696058b09238891aef0e760732bb28f83cf9f
lukehayes <lukepurplehayes@gmail.com>
2020-02-10 21:54:50 +0000

    Cleaned up main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,5 @@
-﻿// VS-CMake-SDL.cpp : Defines the entry point for the application.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include "SDL.h"
-
-int r() {
-	return std::rand() % 255;
-}
 
 int main(int argc, char* argv[])
 {
@@ -16,41 +9,19 @@ int main(int argc, char* argv[])
 
 	SDL_Window* window;
 	SDL_Renderer* renderer;
-
 	SDL_CreateWindowAndRenderer(800, 600, SDL_WINDOW_RESIZABLE, &window, &renderer);
 
-
-	//SDL_Window* window = SDL_CreateWindow
-	//("An SDL2 window", // window's title
-	//	10, 25, // coordinates on the screen, in pixels, of the window's upper left corner
-	//	640, 480, // window's length and height in pixels  
-	//	SDL_WINDOW_RESIZABLE);
-
-	//SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_WINDOW_RESIZABLE) ;
-
 	SDL_Event event;
+
 	while (1) {
-	SDL_Rect rc;
-	rc.x = 10;
-	rc.y = 10;
-	rc.w = r();
-	rc.h = r();
 
 		SDL_PollEvent(&event);
 		if (event.type == SDL_QUIT) {
 			break;
 		}
 
-		std::cout << r() << std::endl;
-
-		SDL_SetRenderDrawColor(renderer, r(), r(), r(), 255);
+		SDL_SetRenderDrawColor(renderer, 10, 10, 10, 255);
 		SDL_RenderClear(renderer);
-
-		SDL_SetRenderDrawColor(renderer, 255, r(), r(), 255);
-
-		SDL_RenderDrawRect(renderer, &rc);
-
-
 		SDL_RenderPresent(renderer);
 	}
 
