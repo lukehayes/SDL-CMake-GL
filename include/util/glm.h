6dfca401da7fa88154b809702aa12925c46c2af2
lukehayes <lukepurplehayes@gmail.com>
2020-02-15 12:14:28 +0000

    Added util/glm.h includes

--- include/util/glm.h
+++ include/util/glm.h
@@ -1,6 +1,10 @@
 #ifndef UTIL_GLM_H
 #define UTIL_GLM_H
 
-#include "glm.hpp"
+/**
+* Include of all of necessary glm header files in this one
+*/
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #endif // !UTIL_GLM_H
