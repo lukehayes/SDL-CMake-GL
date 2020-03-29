f8e4ef922dd0ebbef7d264a944abb60ad06369b4
lukehayes <lukepurplehayes@gmail.com>
2020-03-29 17:48:07 +0100

    Updated main

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 
 #include <iostream>
 #include "SDL.h"
+#include "game/player.h"
+#include "components/input.h"
 
 int main(int argc, char* argv[])
 {
@@ -14,21 +16,39 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
+
 	SDL_Window* window;
 	SDL_Renderer* renderer;
 	SDL_CreateWindowAndRenderer(800, 600, SDL_WINDOW_RESIZABLE, &window, &renderer);
 
+    Input input;
+
+    Player player;
+    SDL_Rect rect;
+    rect.x = player.transform.getPosition().x;
+    rect.y = player.transform.getPosition().y;
+    rect.w = 20;
+    rect.h = 20;
+
+
 	SDL_Event event;
 
 	while (1) {
+        
+        input.update();
+        player.update();
+        rect.x = player.transform.getPosition().x;
+        rect.y = player.transform.getPosition().y;
 
 		SDL_PollEvent(&event);
 		if (event.type == SDL_QUIT) {
 			break;
 		}
 
-		SDL_SetRenderDrawColor(renderer, 0, 150, 200, 255);
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 		SDL_RenderClear(renderer);
+		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderFillRect(renderer, &rect);
 		SDL_RenderPresent(renderer);
 	}
 
