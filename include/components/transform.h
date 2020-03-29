73b31e1c0d3e15b85fd85098a5a4121cce788bdc
lukehayes <lukepurplehayes@gmail.com>
2020-03-29 16:25:04 +0100

    Added Transform component

---
+++ include/components/transform.h
@@ -0,0 +1,46 @@
+#ifndef TRANSFORM_H
+#define TRANSFORM_H
+
+#include <iostream>
+#include "component.h"
+#include "math/vec2.h"
+
+/**
+ * Transform Transform stores position and rotation
+ */
+class Transform : public Component {
+    public:
+        Transform() {}
+        virtual ~Transform() {}
+        void update() {
+            std::cout << "Updating Transform..." << std::endl;
+        }
+        
+        /**
+         * Position Getter
+         *
+         * @return Vec2<float>
+         */
+        V2i getPosition() const {
+            return this->position;
+        }
+
+        /**
+         * Position Setter
+         *
+         * @param x integer
+         * @param y integer
+         *
+         * @return void
+         */
+        void setPosition(int x, int y) {
+            this->position.x = x;
+            this->position.y = y;
+        }
+
+    private:
+        V2i position;
+};
+
+
+#endif
