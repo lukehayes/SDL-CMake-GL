608bb0ae174bc4c58e8748adbf1ab2480b5754e6
lukehayes <lukepurplehayes@gmail.com>
2020-07-16 11:45:32 +0100

    Created App::App class

---
+++ include/app/app.h
@@ -0,0 +1,20 @@
+#ifndef APP_H
+#define APP_H
+
+namespace App {
+
+	class App {
+
+        public:
+		App() {}
+		~App() {}
+        
+        void update(double dt) {}
+
+        void render() {}
+        
+        void run(){}
+
+        private:
+	};
+}
