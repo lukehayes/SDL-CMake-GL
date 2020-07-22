504e774333f33578e70589e81fc59966c15a3e0d
lukehayes <lukepurplehayes@gmail.com>
2020-07-22 12:10:11 +0100

    Moved texture loading into its own function

--- src/main.cpp
+++ src/main.cpp
@@ -51,6 +51,28 @@ void Draw(const std::vector<float>& verticies, const std::vector<unsigned int>& 
 
 }
 
+void LoadTexture(const char* image_path) {
+
+    unsigned int texture;
+    glGenTextures(1, &texture); 
+
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("../assets/images/container.jpg", &width, &height, &nrChannels, 0);
+    std::cout << width << " " << height << std::endl;
+
+    glBindTexture(GL_TEXTURE_2D, texture); 
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    stbi_image_free(data);
+}
+
 int main(int argc, char* argv[])
 {
 
@@ -85,25 +107,7 @@ int main(int argc, char* argv[])
     }
 
 
-    unsigned int texture;
-    glGenTextures(1, &texture); 
-
-    int width, height, nrChannels;
-    unsigned char *data = stbi_load("../assets/images/container.jpg", &width, &height, &nrChannels, 0);
-    std::cout << width << " " << height << std::endl;
-
-    glBindTexture(GL_TEXTURE_2D, texture); 
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-    glGenerateMipmap(GL_TEXTURE_2D);
-
-    stbi_image_free(data);
-
+    LoadTexture("../assets/images/container.jpg");
     Draw(v, i);
 
     app.Run();
