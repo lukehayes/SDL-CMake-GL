42ffeff67f4de38ee6f4daabf2cf058a07c8888b
lukehayes <lukepurplehayes@gmail.com>
2020-07-22 16:55:01 +0100

    Added platform/resource.h for platform specific resource handling

---
+++ include/platform/resource.h
@@ -0,0 +1,37 @@
+/**
+ * Platform specific helper functions to get resources like images.
+ */
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+#ifdef __unix__ 
+
+    /**
+     * Get the path to an image resource.
+     *
+     * @param filename The name of the file to load
+     *
+     * @return The full path to the image resource
+     */
+    std::string ImageResource(const std::string& filename) {
+        std::string path = "../assets/images/" + filename;
+        return path;
+    }
+
+#elif _WIN32
+
+    /**
+     * Get the path to an image resource.
+     *
+     * @param filename The name of the file to load
+     *
+     * @return The full path to the image resource
+     */
+    void ImageResource() {
+        std::cout << "Running on Windows. ImageResource() function needs implementation!" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+#endif
