eaab1d8acae8762f9e4757302bb6460d56e07d54
lukehayes <lukepurplehayes@gmail.com>
2020-08-12 20:02:38 +0100

    Added Graphics::Renderer class.

---
+++ include/Graphics/Renderer.h
@@ -0,0 +1,13 @@
+#ifndef GRAPHICS_RENDERER_H
+#define GRAPHICS_RENDERER_H
+
+namespace Graphics {
+
+	class Renderer {
+	public:
+		Renderer() {}
+		~Renderer() {}
+	};
+}
+
+#endif // !GRAPHICS_RENDERER_H
