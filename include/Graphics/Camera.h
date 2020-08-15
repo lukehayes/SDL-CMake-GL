5e100aa587aea946f678a4339ae0f3c7f1d36e5b
lukehayes <lukepurplehayes@gmail.com>
2020-08-15 10:26:13 +0100

    Initialzed Graphcis::Camera members correctly.

--- include/Graphics/Camera.h
+++ include/Graphics/Camera.h
@@ -3,11 +3,12 @@
 
 #include "../Util/glm.h"
 
+
 namespace Graphics {
 
 	class Camera {
 	public:
-		Camera() : m_position(0,0,0} ) {}
+		Camera() : m_position(0,0,-3.0)  {}
 		Camera(const Camera& cam) {}
 		Camera(const glm::vec3& pos) : m_position(pos) {}
 		~Camera() {}
@@ -17,18 +18,18 @@ namespace Graphics {
 		*/
 		void Setup() {
 			m_projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
-			m_view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+			m_view = glm::translate(m_view, glm::vec3(0.0f, 0.0f, -3.0f));
 		}
 
 		glm::mat4 GetCombinedProjection() {
 			return m_projection * m_view;
 		}
 
-		Update(double dt) {}
+		void Update(double dt) {}
 
-		glm::vec3 m_position;
-		glm::mat4 m_projection(1.0f);
-		glm::mat4 m_view(1.0f);
+		glm::vec3 m_position = glm::vec3(0.0f);
+		glm::mat4 m_projection = glm::mat4(1.0f);
+		glm::mat4 m_view =	glm::mat4(1.0f);
 	};
 }
 
