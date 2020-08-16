2ec3724f95c10b2b3856e8823b16dead121a30f0
lukehayes <lukepurplehayes@gmail.com>
2020-08-16 21:16:56 +0100

    Added Model/BaseMesh.h

---
+++ include/Model/BaseMesh.h
@@ -0,0 +1,26 @@
+#ifdef MODEL_BASE_MESH_H
+#define MODEL_BASE_MESH_H
+
+#include <vector>
+
+
+namespace Model {
+
+    /**
+     * Base class represents all meshes and is meant to be inheritied from.
+     */
+    class BaseMesh
+    {
+    public:
+        BaseMesh() {}
+        virtual ~BaseMesh(){}
+    protected:
+
+    };
+
+    
+} /* namespace Model */
+
+
+
+#endif
