9a2840c428ccd1f2aee0e0a1f76c3ebe6587f4f8
lukehayes <lukepurplehayes@gmail.com>
2020-08-23 11:32:01 +0100

    Renamed m_model_matrix to m_modelMatrix

--- include/Model/Model.h
+++ include/Model/Model.h
@@ -8,19 +8,24 @@ namespace Model {
 	class Model
 	{
 	public:
-		Model() : m_position(0,0,0){}
+		Model() : m_position(0,1,0){}
 		Model(const glm::vec3 &position) : m_position(position) {}
 		~Model() {}
 
 		inline glm::mat4& GetMatrix()
 		{
-			return m_model_matrix;
+			return m_modelMatrix;
 		}
+
+        void Update(double delta)
+        {
+            m_modelMatrix = glm::translate(m_modelMatrix, m_position);
+        }
 
 		glm::vec3 m_position = glm::vec3(1.0f);
 
 	private:
-		glm::mat4 m_model_matrix = glm::mat4(1.0f);
+		glm::mat4 m_modelMatrix = glm::mat4(1.0f);
 
 	};
 
