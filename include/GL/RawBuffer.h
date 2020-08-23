efba7cf29494ac5ba954c53eddef633c9bdfd91b
lukehayes <lukepurplehayes@gmail.com>
2020-08-23 18:25:25 +0100

    Added comments to buffers to make them clearer as to what they are doing.

--- include/GL/RawBuffer.h
+++ include/GL/RawBuffer.h
@@ -34,18 +34,19 @@ namespace GL {
 
                 glBufferData(GL::BufferTarget::ARRAY_BUFFER, sizeof(float) * verticies.size(), verticies.data(), GL_STATIC_DRAW );
 
+                // Vertex Coordinates
                 glEnableVertexAttribArray(0); // Turn attrib 0 on
-                // 6 verticies in total. 3 are positions, 3 are colours.
                 glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)0);
 
+                // Colour Coordinates
                 glEnableVertexAttribArray(1); // Turn attrib 1 on
-                // Same as attrib 0 but we need to jump 12 bytes (3 * float) to get to
-                // the colour attribs. Then every six verts is our next colour.
                 glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(3 * sizeof(float)));
 
+                // Texture Coordinates
                 glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
                 glEnableVertexAttribArray(2);  
 
+                // Index Buffer
                 glGenBuffers(1, &m_EBO_ID);
                 glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO_ID);
                 glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indicies.size(), indicies.data(), GL_STATIC_DRAW );
