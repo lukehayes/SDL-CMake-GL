e7c32547c5503fc4c4a88645eec17b53e218a03f
lukehayes <lukepurplehayes@gmail.com>
2020-09-03 16:02:41 +0100

    Added default shaders to project.

---
+++ res/shaders/default-vsh.glsl
@@ -0,0 +1,15 @@
+#version 330 core
+layout (location = 0) in vec3 vtxPosition;
+layout (location = 1) in vec3 vtxColor;
+layout (location = 2) in vec2 vtxTexCoord;
+  
+out vec4 vertexColor;
+
+uniform mat4 projection;
+uniform mat4 model;
+
+void main()
+{
+    gl_Position = projection * model * vec4(vtxPosition, 1.0);
+    vertexColor = vec4(vtxColor, 1.0); 
+}
