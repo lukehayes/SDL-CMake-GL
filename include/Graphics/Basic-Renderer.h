773e22136b80204b67201c75ebaa60ae52cb5bf2
lukehayes <lukepurplehayes@gmail.com>
2020-09-04 12:43:46 +0100

    Added GL::Shader member to GL::BasicRenderer class.

--- include/Graphics/Basic-Renderer.h
+++ include/Graphics/Basic-Renderer.h
@@ -5,26 +5,40 @@
 #include "GL/Shader.h"
 #include "Util/GL.h"
 #include "Model/Model.h"
+#include "App/EngineBase.h"
 
 namespace Graphics {
 
 	class BasicRenderer {
 	public:
 		BasicRenderer() {}
+
+		BasicRenderer(const GL::Shader& shader) 
+			: m_shader(shader){}
+
 		BasicRenderer(const Model::Model& model) : m_model(model){}
-		BasicRenderer(const GL::RawBuffer& buffer, const Model::Model model)
-			: m_rawBuffer(buffer), m_model(model) {}
+		BasicRenderer(const GL::RawBuffer& buffer, const Model::Model model, const GL::Shader& shader)
+			: m_rawBuffer(buffer), m_model(model), m_shader(shader) {}
 		~BasicRenderer() {}
 
-		void Render(const GL::Shader& shader)
+		void SetShader()
 		{
-			shader.Use();
+            const std::string vshPath = ShaderResource("default-vsh.glsl");
+            const std::string fshPath = ShaderResource("default-fsh.glsl");
+            GL::Shader shader(vshPath.c_str(), fshPath.c_str());
+			m_shader = shader;
+		}
+
+		void Render()
+		{
+			m_shader.Use();
 			glDrawElements(GL_TRIANGLES, m_model.m_vertexCount, GL_UNSIGNED_INT, 0);
 		}
 
 	private:
 		GL::RawBuffer m_rawBuffer;
 		Model::Model m_model;
+		GL::Shader m_shader;
 	};
 }
 
