cb0eafc3c917a095697b02561a93a5fa2c5dd376
lukehayes <lukepurplehayes@gmail.com>
2020-10-14 09:15:32 +0100

    Reversed direction projection Z axis moves in.

--- include/Graphics/Camera.h
+++ include/Graphics/Camera.h
@@ -8,7 +8,7 @@ namespace Graphics {
 
 	class Camera {
 	public:
-        Camera() : m_position(0.0,0.0,-3.0f)  
+        Camera() : m_position(0.0,0.0,3.0f)  
         {
             Setup();
         }
