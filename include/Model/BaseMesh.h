c4ae597811107d07abd3ce860b76468386d59170
lukehayes <lukepurplehayes@gmail.com>
2020-10-17 16:24:04 +0100

    Added BaseMesh::SetVerticies and BaseMesh::SetIndicies methods

--- include/Model/BaseMesh.h
+++ include/Model/BaseMesh.h
@@ -14,6 +14,9 @@ namespace Model {
         BaseMesh() {}
         virtual ~BaseMesh(){}
 
+        virtual void SetVertices() = 0;
+        virtual void SetIndicies() = 0;
+
     protected:
         std::vector<float> m_verticies;
         std::vector<unsigned int> m_indcies;
